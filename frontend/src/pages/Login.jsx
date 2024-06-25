08f5f87facc7f2fdb1ff2ff821bcd6a85c8712be
kanak.dodiya <kanak.dodiya@isyncevolution.com>
2024-06-25 18:59:58 +0530

    [Add] Css

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,33 @@
+// import React from 'react'
+
+import {
+  Avatar,
+  Button,
+  Container,
+  IconButton,
+  Paper,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+
+const Login = () => {
+  return (
+    <div>
+      <Container
+        component={'main'}
+        maxWidth="xs"
+        sx={{
+          height: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+
+      </Container>
+    </div>
+  )
+}
+
+export default Login
