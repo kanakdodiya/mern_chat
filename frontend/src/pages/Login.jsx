344066572355aece173b8c73c056549ad8db8045
kanakdodiya <kanakdodiya30@gmail.com>
2024-06-26 08:57:44 +0530

    [Add] login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,5 @@
 // import React from 'react'
-
+import { CameraAlt as CameraAltIcon } from "@mui/icons-material";
 import {
   Avatar,
   Button,
@@ -10,8 +10,13 @@ import {
   TextField,
   Typography,
 } from "@mui/material";
+import { useState } from "react";
+import { VisuallyHiddenInput } from "../components/styles/StyledComponents";
 
 const Login = () => {
+  const [isLogin, setIsLogin] = useState(true);
+
+  const toggleLogin = () => setIsLogin((prev) => !prev)
   return (
     <div>
       <Container
@@ -24,6 +29,118 @@ const Login = () => {
           alignItems: "center",
         }}
       >
+        <Paper
+          elevation={3}
+          sx={{
+            padding: 4,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          {isLogin ? (
+            <>
+              <Typography variant="h5">Login</Typography>
+              <form action="">
+                <TextField
+                  required
+                  fullWidth
+                  label="Username"
+                  margin="normal"
+                  variant="outlined"
+                // value={username.value}
+                // onChange={username.changeHandler}
+                />
+
+                <TextField
+                  required
+                  fullWidth
+                  label="Password"
+                  type="password"
+                  margin="normal"
+                  variant="outlined"
+                // value={password.value}
+                // onChange={password.changeHandler}
+                />
+
+                <Button
+                  sx={{
+                    marginTop: "1rem",
+                  }}
+                  variant="contained"
+                  color="primary"
+                  type="submit"
+                  fullWidth
+                // disabled={isLoading}
+                >
+                  Login
+
+
+                </Button>
+
+                <Typography textAlign={"center"} m={"1rem"}>
+                  OR
+                </Typography>
+
+                <Button
+                  // disabled={isLoading}
+                  fullWidth
+                  variant="text"
+                  onClick={toggleLogin}
+                >
+                  Sign Up Instead
+                </Button>
+              </form>
+            </>
+          ) : (
+            <>
+              <Typography variant="h5">Sign Up</Typography>
+              <form
+                style={{
+                  width: "100%",
+                  marginTop: "1rem",
+                }}
+
+              // onSubmit={handleSignUp}
+              >
+                <Stack position={"relative"} width={"10rem"} margin={"auto"}>
+
+                  <Avatar
+                    sx={{
+                      width: "10rem",
+                      height: "10rem",
+                      objectFit: "contain",
+                    }}
+                  // src={avatar.preview}
+                  />
+
+                  <IconButton
+                    sx={{
+                      position: "absolute",
+                      bottom: "0",
+                      right: "0",
+                      color: "white",
+                      bgcolor: "rgba(0,0,0,0.5)",
+                      ":hover": {
+                        bgcolor: "rgba(0,0,0,0.7)",
+                      },
+                    }}
+                    component="label"
+                  >
+                    <CameraAltIcon />
+                    <VisuallyHiddenInput
+                      type="file"
+                    // onChange={avatar.changeHandler}
+                    />
+                  </IconButton>
+                </Stack>
+
+
+              </form>
+
+            </>
+          )}
+        </Paper>
 
       </Container>
     </div>
